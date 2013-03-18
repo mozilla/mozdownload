d7ca436c550a84a8bbb660e5ad2cc08fed648f98
Johannes Vogel <mozilla_dev@nebelhom.com>
2013-03-19 00:23:32 +0100

    Made the pull request pep8 compliant

--- mozdownload/scraper.py
+++ mozdownload/scraper.py
@@ -187,24 +187,24 @@ class Scraper(object):
 
     def update_download_progress(self, bytes_downloaded, total_size, dl_rate, eta):
         """Updates the download progressbar for the download method"""
-        
+
         percent = (bytes_downloaded / total_size) * 100
         progressbar_string = '=' * int(percent / 100 * 39) + '>'
         bytes_formatted = locale.format('%d', bytes_downloaded, grouping=True)
-        
-        sys.stdout.write('%3d%% [%-40s] %11s %4.1f MB/s %4.1f s \r' % 
+
+        sys.stdout.write('%3d%% [%-40s] %11s %4.1f MB/s %4.1f s \r' %
                         (percent, progressbar_string, bytes_formatted, dl_rate, eta))
         sys.stdout.flush()
         if percent >= 100:
             sys.stdout.write('\n')
-            
+
     def get_download_rate_and_eta(self, start_time, time_so_far, bytes_downloaded,
                                   total_size):
         """Returns the current dowload rate in MB/s and estimated duration"""
-        
-        dl_rate = (bytes_downloaded / (time_so_far-start_time).total_seconds())  # in bytes
+
+        dl_rate = (bytes_downloaded / (time_so_far - start_time).total_seconds())  # in bytes
         eta = (total_size - bytes_downloaded) / (dl_rate * 1.0)  # estimated remaining time in seconds
-        return dl_rate / (1024**2), eta
+        return dl_rate / (1024 ** 2), eta
 
     def download(self):
         """Download the specified file"""
