cc80ea1f93b5c06a83e71b2a489018316f8f43c7
Johannes Vogel <mozilla_dev@nebelhom.com>
2013-04-15 11:30:21 +0200

    Changed the inline comments

--- mozdownload/scraper.py
+++ mozdownload/scraper.py
@@ -117,7 +117,8 @@ class Scraper(object):
                     raise NotFoundException("Binary not found in folder",
                                             self.path)
             except (NotFoundException, urllib2.HTTPError):
-                if self.retry_attempts > 0:  # Otherwise shows printout but no retry is done
+                # Otherwise shows printout but no retry is done
+                if self.retry_attempts > 0:
                     print "Binary not found! Retrying... (attempt %s)" % attempt
                 if attempt >= self.retry_attempts:
                     raise NotFoundException("Binary not found in folder",
@@ -242,7 +243,8 @@ class Scraper(object):
             except (urllib2.HTTPError, urllib2.URLError, TimeoutException):
                 if tmp_file and os.path.isfile(tmp_file):
                     os.remove(tmp_file)
-                if self.retry_attempts > 0: # Otherwise shows printout but no retry is done
+                # Otherwise shows printout but no retry is done
+                if self.retry_attempts > 0:
                     print '\nDownload failed! Retrying... (attempt %s)' % attempt
                 if attempt >= self.retry_attempts:
                     raise
