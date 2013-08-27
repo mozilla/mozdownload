3545402c75ec197b173a3c5e6a18638ab29a7e80
Johannes Vogel <mozilla_dev@nebelhom.com>
2013-08-27 14:15:40 +0200

    Implemented suggestions

--- mozdownload/scraper.py
+++ mozdownload/scraper.py
@@ -81,7 +81,7 @@ class Scraper(object):
     def __init__(self, directory, version, platform=None,
                  application='firefox', locale='en-US', extension=None,
                  authentication=None, retry_attempts=0, retry_delay=10.,
-                 stub=False, timeout=None):
+                 is_stub_installer=False, timeout=None):
 
         # Private properties for caching
         self._target = None
@@ -95,7 +95,7 @@ class Scraper(object):
         self.authentication = authentication
         self.retry_attempts = retry_attempts
         self.retry_delay = retry_delay
-        self.stub = stub
+        self.is_stub_installer = is_stub_installer
         self.timeout_download = timeout
         self.timeout_network = 60.
 
@@ -420,17 +420,15 @@ class DailyScraper(Scraper):
                         'linux64': r'\.%(EXT)s$',
                         'mac': r'\.%(EXT)s$',
                         'mac64': r'\.%(EXT)s$',
-                        'win32': r'(\.installer)%(STUB)\.%(EXT)s$' % {
-                        '-stub' if self.stub else ''},
-                        'win64': r'(\.installer)%(STUB)\.%(EXT)s$' % {
-                        '-stub' if self.stub else ''}
-                        }
+                        'win32': r'(\.installer)%(STUB)\.%(EXT)s$',
+                        'win64': r'(\.installer)%(STUB)\.%(EXT)s$'}
         regex = regex_base_name + regex_suffix[self.platform]
 
         return regex % {'APP': self.application,
                         'LOCALE': self.locale,
                         'PLATFORM': self.platform_regex,
-                        'EXT': self.extension}
+                        'EXT': self.extension,
+                        'STUB': '-stub' if self.is_stub_installer else ''}
 
     def build_filename(self, binary):
         """Return the proposed filename with extension for the binary"""
@@ -504,10 +502,11 @@ class ReleaseScraper(Scraper):
                  'linux64': r'^%(APP)s-.*\.%(EXT)s$',
                  'mac': r'^%(APP)s.*\.%(EXT)s$',
                  'mac64': r'^%(APP)s.*\.%(EXT)s$',
-                 'win32': r'^%(APP)s.*Stub.*\.%(EXT)s$' if self.stub else r'^%(APP)s.*\.%(EXT)s$',
-                 'win64': r'^%(APP)s.*Stub.*\.%(EXT)s$' if self.stub else r'^%(APP)s.*\.%(EXT)s$'}
+                 'win32': r'^%(APP)s.*%(STUB)s.*\.%(EXT)s$',
+                 'win64': r'^%(APP)s.*%(STUB)s.*\.%(EXT)s$'}
         return regex[self.platform] % {'APP': self.application,
-                                       'EXT': self.extension}
+                                       'EXT': self.extension,
+                                       'STUB': 'Stub' if self.is_stub_installer else ''}
 
     @property
     def path_regex(self):
@@ -828,7 +827,7 @@ def cli():
                       metavar='PLATFORM',
                       help='Platform of the application')
     parser.add_option('--stub',
-                      dest='stub',
+                      dest='is_stub_installer',
                       action='store_true',
                       help='Stub installer. '
                            'Only applicable to Windows builds.')
@@ -941,7 +940,7 @@ def cli():
                         'authentication': (options.username, options.password),
                         'retry_attempts': options.retry_attempts,
                         'retry_delay': options.retry_delay,
-                        'stub': options.stub,
+                        'is_stub_installer': options.is_stub_installer,
                         'timeout': options.timeout}
     scraper_options = {
         'candidate': {'build_number': options.build_number,
