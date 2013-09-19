e368cd0166e61b85dd62290479caf83e93770388
Johannes Vogel <mozilla_dev@nebelhom.com>
2013-09-19 17:36:24 +0200

    removed test for type and addressed nitpicks

--- tests/release_scraper/test_release_scraper.py
+++ tests/release_scraper/test_release_scraper.py
@@ -10,75 +10,59 @@ import unittest
 from mozdownload import ReleaseScraper
 import mozhttpd_base_test as mhttpd
 
-BUILD_TYPES = {'release': ReleaseScraper}
-
 firefox_tests = [
     # -p win32 -v latest
     {'args': {'platform': 'win32',
               'version': 'latest'},
-    'target': 'firefox-latest.en-US.win32.exe'},
-    # -t release -p win32 -v latest
-    {'args': {'platform': 'win32',
-              'version': 'latest'},
-    'target': 'firefox-latest.en-US.win32.exe',
-    'type': 'release'},
+     'target': 'firefox-latest.en-US.win32.exe'},
     # -a firefox -p win32 -v latest
     {'args': {'application': 'firefox',
               'platform': 'win32',
               'version': 'latest'},
-    'target': 'firefox-latest.en-US.win32.exe',
-    'type': 'release'},
+     'target': 'firefox-latest.en-US.win32.exe'},
     # -a firefox -t release -p linux -v latest
     {'args': {'application': 'firefox',
               'platform': 'linux',
               'version': 'latest'},
-    'target': 'firefox-latest.en-US.linux.tar.bz2',
-    'type': 'release'},
+     'target': 'firefox-latest.en-US.linux.tar.bz2'},
     # -a firefox -t release -p linux64 -v latest
     {'args': {'application': 'firefox',
               'platform': 'linux64',
               'version': 'latest'},
-    'target': 'firefox-latest.en-US.linux64.tar.bz2',
-    'type': 'release'},
+     'target': 'firefox-latest.en-US.linux64.tar.bz2'},
     # -a firefox -t release -p mac -v latest
     {'args': {'application': 'firefox',
               'platform': 'mac',
               'version': 'latest'},
-    'target': 'firefox-latest.en-US.mac.dmg',
-    'type': 'release'},
+     'target': 'firefox-latest.en-US.mac.dmg'},
     # -a firefox -t release -p win32 -v latest
     {'args': {'application': 'firefox',
               'platform': 'win32',
               'version': 'latest'},
-    'target': 'firefox-latest.en-US.win32.exe',
-    'type': 'release'},
+     'target': 'firefox-latest.en-US.win32.exe'},
     # -a firefox -t release -p win32 -v latest -l de
     {'args': {'application': 'firefox',
               'locale': 'de',
               'platform': 'win32',
               'version': 'latest'},
-    'target': 'firefox-latest.de.win32.exe',
-    'type': 'release'},
+     'target': 'firefox-latest.de.win32.exe'},
     # -a firefox -t release -p win32 -v latest --stub
     {'args': {'application': 'firefox',
               'platform': 'win32',
               'is_stub_installer': True,
               'version': 'latest'},
-     'target': 'firefox-latest.en-US.win32.exe',
-     'type': 'release'},
+     'target': 'firefox-latest.en-US.win32.exe'},
     # -a firefox -t release -p win32 -v 21.0
     {'args': {'application': 'firefox',
               'platform': 'win32',
               'version': '21.0'},
-     'target': 'firefox-21.0.en-US.win32.exe',
-     'type': 'release'},
+     'target': 'firefox-21.0.en-US.win32.exe'},
     # -a firefox -t release -p win32 -v 21.0 -l es-ES
     {'args': {'application': 'firefox',
               'locale': 'es-ES',
               'platform': 'win32',
               'version': '21.0'},
-     'target': 'firefox-21.0.es-ES.win32.exe',
-     'type': 'release'}
+     'target': 'firefox-21.0.es-ES.win32.exe'}
 ]
 
 thunderbird_tests = [
@@ -87,50 +71,38 @@ thunderbird_tests = [
               'platform': 'win32',
               'version': 'latest'},
      'target': 'thunderbird-latest.en-US.win32.exe'},
-    # -a thunderbird -t release -p linux -v latest
-    {'args': {'application': 'thunderbird',
-              'platform': 'linux',
-              'version': 'latest'},
-     'target': 'thunderbird-latest.en-US.linux.tar.bz2',
-     'type': 'release'},
     # -a thunderbird -t release -p linux64 -v latest
     {'args': {'application': 'thunderbird',
               'platform': 'linux64',
               'version': 'latest'},
-     'target': 'thunderbird-latest.en-US.linux64.tar.bz2',
-     'type': 'release'},
+     'target': 'thunderbird-latest.en-US.linux64.tar.bz2'},
     # -a thunderbird -t release -p mac -v latest
     {'args': {'application': 'thunderbird',
               'platform': 'mac',
               'version': 'latest'},
-     'target': 'thunderbird-latest.en-US.mac.dmg',
-     'type': 'release'},
+     'target': 'thunderbird-latest.en-US.mac.dmg'},
     # -a thunderbird -t release -p win32 -v latest
     {'args': {'application': 'thunderbird',
               'platform': 'win32',
               'version': 'latest'},
-     'target': 'thunderbird-latest.en-US.win32.exe',
-     'type': 'release'},
+     'target': 'thunderbird-latest.en-US.win32.exe'},
     # -a thunderbird -t release -p win32 -v latest -l de
     {'args': {'application': 'thunderbird',
               'locale': 'de',
               'platform': 'win32',
               'version': 'latest'},
-     'target': 'thunderbird-latest.de.win32.exe',
-     'type': 'release'},
+     'target': 'thunderbird-latest.de.win32.exe'},
     # -a thunderbird -t release -p win32 -v 16.0
     {'args': {'application': 'thunderbird',
               'platform': 'win32',
               'version': '16.0'},
-     'target': 'thunderbird-16.0.en-US.win32.exe',
-     'type': 'release'},
+     'target': 'thunderbird-16.0.en-US.win32.exe'},
     # -a thunderbird -t release -p win32 -v 16.0 -l es-ES
     {'args': {'application': 'thunderbird',
               'locale': 'es-ES',
               'platform': 'win32',
               'version': '16.0'},
-     'target': 'thunderbird-16.0.es-ES.win32.exe',
-     'type': 'release'}
+     'target': 'thunderbird-16.0.es-ES.win32.exe'}
 ]
 
 tests = firefox_tests + thunderbird_tests
@@ -143,17 +115,10 @@ class ReleaseScraperTest(mhttpd.MozHttpdBaseTest):
         """Testing various download scenarios for ReleaseScraper"""
 
         for entry in tests:
-            # Mimick real case scenario
-            if entry.get('type'):
-                scraper = BUILD_TYPES[entry['type']](directory=self.temp_dir,
-                                                     base_url=self.wdir,
-                                                     **entry['args'])
-            else:
-                scraper = ReleaseScraper(directory=self.temp_dir,
-                                         base_url=self.wdir, **entry['args'])
-
-            target_cmp = os.path.join(self.temp_dir, entry['target'])
-            self.assertEqual(scraper.target, target_cmp)
+            scraper = ReleaseScraper(directory=self.temp_dir, base_url=self.wdir,
+                                     **entry['args'])
+            expected_target = os.path.join(self.temp_dir, entry['target'])
+            self.assertEqual(scraper.target, expected_target)
 
 if __name__ == '__main__':
     unittest.main()
