7834908e09b7c675342fabc6b2aefe69524c93ed
Johannes Vogel <mozilla_dev@nebelhom.com>
2013-09-28 18:52:47 +0200

    Removed -t candidate from comments

--- tests/candidate_scraper/test_candidate_scraper.py
+++ tests/candidate_scraper/test_candidate_scraper.py
@@ -11,49 +11,49 @@ from mozdownload import ReleaseCandidateScraper
 import mozhttpd_base_test as mhttpd
 
 firefox_tests = [
-    # -t candidate -p win32 -v 21.0
+    # -p win32 -v 21.0
     {'args': {'platform': 'win32',
               'version': '21.0'},
      'target': 'firefox-21.0-build3.en-US.win32.exe'},
-    # -a firefox -t candidate -p linux -v 21.0
+    # -a firefox -p linux -v 21.0
     {'args': {'application': 'firefox',
               'platform': 'linux',
               'version': '21.0'},
      'target': 'firefox-21.0-build3.en-US.linux.tar.bz2'},
-    # -a firefox -t candidate -p linux64 -v 21.0
+    # -a firefox -p linux64 -v 21.0
     {'args': {'application': 'firefox',
               'platform': 'linux64',
               'version': '21.0'},
      'target': 'firefox-21.0-build3.en-US.linux64.tar.bz2'},
-    # -a firefox -t candidate -p mac -v 21.0
+    # -a firefox -p mac -v 21.0
     {'args': {'application': 'firefox',
               'platform': 'mac',
               'version': '21.0'},
      'target': 'firefox-21.0-build3.en-US.mac.dmg'},
-    # -a firefox -t candidate -p win32 -v 21.0
+    # -a firefox -p win32 -v 21.0
     {'args': {'application': 'firefox',
               'platform': 'win32',
               'version': '21.0'},
      'target': 'firefox-21.0-build3.en-US.win32.exe'},
-    # -a firefox -t candidate -p win32 -v 21.0 -l cs
+    # -a firefox -p win32 -v 21.0 -l cs
     {'args': {'application': 'firefox',
               'locale': 'cs',
               'platform': 'win32',
               'version': '21.0'},
      'target': 'firefox-21.0-build3.cs.win32.exe'},
-    # -a firefox -t candidate -p win32 -v 21.0 -l en-GB
+    # -a firefox -p win32 -v 21.0 -l en-GB
     {'args': {'application': 'firefox',
               'locale': 'en-GB',
               'platform': 'win32',
               'version': '21.0'},
      'target': 'firefox-21.0-build3.en-GB.win32.exe'},
-    # -a firefox -t candidate -p win32 -v 21.0 --build-number=1
+    # -a firefox -p win32 -v 21.0 --build-number=1
     {'args': {'application': 'firefox',
               'build_number': '1',
               'platform': 'win32',
               'version': '21.0'},
      'target': 'firefox-21.0-build1.en-US.win32.exe'},
-    # -a firefox -t candidate -p win32 -v 21.0 --stub
+    # -a firefox -p win32 -v 21.0 --stub
     {'args': {'application': 'firefox',
               'is_stub_installer': True,
               'platform': 'win32',
@@ -62,39 +62,39 @@ firefox_tests = [
 ]
 
 thunderbird_tests = [
-    # -a thunderbird -t candidate -p linux -v 10.0.5esr
+    # -a thunderbird -p linux -v 10.0.5esr
     {'args': {'application': 'thunderbird',
               'platform': 'linux',
               'version': '10.0.5esr'},
      'target': 'thunderbird-10.0.5esr-build3.en-US.linux.tar.bz2'},
-    # -a thunderbird -t candidate -p linux64 -v 10.0.5esr
+    # -a thunderbird -p linux64 -v 10.0.5esr
     {'args': {'application': 'thunderbird',
               'platform': 'linux64',
               'version': '10.0.5esr'},
      'target': 'thunderbird-10.0.5esr-build3.en-US.linux64.tar.bz2'},
-    # -a thunderbird -t candidate -p mac -v 10.0.5esr
+    # -a thunderbird -p mac -v 10.0.5esr
     {'args': {'application': 'thunderbird',
               'platform': 'mac',
               'version': '10.0.5esr'},
      'target': 'thunderbird-10.0.5esr-build3.en-US.mac.dmg'},
-    # -a thunderbird -t candidate -p win32 -v 10.0.5esr
+    # -a thunderbird -p win32 -v 10.0.5esr
     {'args': {'application': 'thunderbird',
               'platform': 'win32',
               'version': '10.0.5esr'},
      'target': 'thunderbird-10.0.5esr-build3.en-US.win32.exe'},
-    # -a thunderbird -t candidate -p win32 -v 10.0.5esr -l cs
+    # -a thunderbird -p win32 -v 10.0.5esr -l cs
     {'args': {'application': 'thunderbird',
               'locale': 'cs',
               'platform': 'win32',
               'version': '10.0.5esr'},
      'target': 'thunderbird-10.0.5esr-build3.cs.win32.exe'},
-    # -a thunderbird -t candidate -p win32 -v 10.0.5esr -l en-GB
+    # -a thunderbird -p win32 -v 10.0.5esr -l en-GB
     {'args': {'application': 'thunderbird',
               'locale': 'en-GB',
               'platform': 'win32',
               'version': '10.0.5esr'},
      'target': 'thunderbird-10.0.5esr-build3.en-GB.win32.exe'},
-    # -a thunderbird -t candidate -p win32 -v 10.0.5esr
+    # -a thunderbird -p win32 -v 10.0.5esr
     {'args': {'application': 'thunderbird',
               'build_number': '1',
               'platform': 'win32',
@@ -112,8 +112,9 @@ class ReleaseScraperTest(mhttpd.MozHttpdBaseTest):
         """Testing various download scenarios for ReleaseScraper"""
 
         for entry in tests:
-            scraper = ReleaseCandidateScraper(directory=self.temp_dir, base_url=self.wdir,
-                                     **entry['args'])
+            scraper = ReleaseCandidateScraper(directory=self.temp_dir,
+                                              base_url=self.wdir,
+                                              **entry['args'])
             expected_target = os.path.join(self.temp_dir, entry['target'])
             self.assertEqual(scraper.target, expected_target)
 
