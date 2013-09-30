17213423aaad66db5c6618869bf347b8e729b014
Johannes Vogel <mozilla_dev@nebelhom.com>
2013-09-30 19:39:18 +0200

    Moved tests back out and hardcode path

--- tests/release_scraper/test_release_scraper.py
+++ tests/release_scraper/test_release_scraper.py
@@ -12,6 +12,114 @@ from mozdownload import ReleaseScraper
 from mozdownload.utils import urljoin
 import mozhttpd_base_test as mhttpd
 
+firefox_tests = [
+    # -p win32 -v latest
+    {'args': {'platform': 'win32',
+              'version': 'latest'},
+     'target': 'firefox-latest.en-US.win32.exe',
+     'target_url': 'firefox/releases/latest/win32/en-US/Firefox Setup 23.0.1.exe'},
+    # -a firefox -p win32 -v latest
+    {'args': {'application': 'firefox',
+              'platform': 'win32',
+              'version': 'latest'},
+     'target': 'firefox-latest.en-US.win32.exe',
+     'target_url': 'firefox/releases/latest/win32/en-US/Firefox Setup 23.0.1.exe'},
+    # -a firefox -p linux -v latest
+    {'args': {'application': 'firefox',
+              'platform': 'linux',
+              'version': 'latest'},
+     'target': 'firefox-latest.en-US.linux.tar.bz2',
+     'target_url': 'firefox/releases/latest/linux-i686/en-US/firefox-23.en-US.linux.tar.bz2'},
+    # -a firefox -p linux64 -v latest
+    {'args': {'application': 'firefox',
+              'platform': 'linux64',
+              'version': 'latest'},
+     'target': 'firefox-latest.en-US.linux64.tar.bz2',
+     'target_url': 'firefox/releases/latest/linux-x86_64/en-US/firefox-23.en-US.linux.tar.bz2'},
+    # -a firefox -p mac -v latest
+    {'args': {'application': 'firefox',
+              'platform': 'mac',
+              'version': 'latest'},
+     'target': 'firefox-latest.en-US.mac.dmg',
+     'target_url': 'firefox/releases/latest/mac/en-US/firefox-23.dmg'},
+    # -a firefox -p win32 -v latest -l de
+    {'args': {'application': 'firefox',
+              'locale': 'de',
+              'platform': 'win32',
+              'version': 'latest'},
+     'target': 'firefox-latest.de.win32.exe',
+     'target_url': 'firefox/releases/latest/win32/de/Firefox Setup 23.0.1.exe'},
+    # -a firefox -p win32 -v latest --stub
+    {'args': {'application': 'firefox',
+              'platform': 'win32',
+              'is_stub_installer': True,
+              'version': 'latest'},
+     'target': 'firefox-latest.en-US.win32.exe',
+     'target_url': 'firefox/releases/latest/win32/en-US/Firefox Setup Stub 23.0.1.exe'},
+    # -a firefox -p win32 -v 21.0
+    {'args': {'application': 'firefox',
+              'platform': 'win32',
+              'version': '21.0'},
+     'target': 'firefox-21.0.en-US.win32.exe',
+     'target_url': 'firefox/releases/21.0/win32/en-US/Firefox Setup 21.0.exe'},
+    # -a firefox -p win32 -v 21.0 -l es-ES
+    {'args': {'application': 'firefox',
+              'locale': 'es-ES',
+              'platform': 'win32',
+              'version': '21.0'},
+     'target': 'firefox-21.0.es-ES.win32.exe',
+     'target_url': 'firefox/releases/21.0/win32/es-ES/Firefox Setup 21.0.exe'}
+]
+
+thunderbird_tests = [
+    # -a thunderbird -p win32 -v latest
+    {'args': {'application': 'thunderbird',
+              'platform': 'win32',
+              'version': 'latest'},
+     'target': 'thunderbird-latest.en-US.win32.exe',
+     'target_url': 'thunderbird/releases/latest/win32/en-US/Thunderbird Setup 17.0.1.exe'},
+    # -a thunderbird -p linux -v latest
+    {'args': {'application': 'thunderbird',
+              'platform': 'linux',
+              'version': 'latest'},
+     'target': 'thunderbird-latest.en-US.linux.tar.bz2',
+     'target_url': 'thunderbird/releases/latest/linux-i686/en-US/thunderbird-17.0.en-US.linux.tar.bz2'},
+    # -a thunderbird -p linux64 -v latest
+    {'args': {'application': 'thunderbird',
+              'platform': 'linux64',
+              'version': 'latest'},
+     'target': 'thunderbird-latest.en-US.linux64.tar.bz2',
+     'target_url': 'thunderbird/releases/latest/linux-x86_64/en-US/thunderbird-17.0.en-US.linux.tar.bz2'},
+    # -a thunderbird -p mac -v latest
+    {'args': {'application': 'thunderbird',
+              'platform': 'mac',
+              'version': 'latest'},
+     'target': 'thunderbird-latest.en-US.mac.dmg',
+     'target_url': 'thunderbird/releases/latest/mac/en-US/thunderbird-17.0.dmg'},
+    # -a thunderbird -p win32 -v latest -l de
+    {'args': {'application': 'thunderbird',
+              'locale': 'de',
+              'platform': 'win32',
+              'version': 'latest'},
+     'target': 'thunderbird-latest.de.win32.exe',
+     'target_url': 'thunderbird/releases/latest/win32/de/Thunderbird Setup 17.0.1.exe'},
+    # -a thunderbird -p win32 -v 16.0
+    {'args': {'application': 'thunderbird',
+              'platform': 'win32',
+              'version': '16.0'},
+     'target': 'thunderbird-16.0.en-US.win32.exe',
+     'target_url': 'thunderbird/releases/16.0/win32/en-US/Thunderbird Setup 16.0.exe'},
+    # -a thunderbird -p win32 -v 16.0 -l es-ES
+    {'args': {'application': 'thunderbird',
+              'locale': 'es-ES',
+              'platform': 'win32',
+              'version': '16.0'},
+     'target': 'thunderbird-16.0.es-ES.win32.exe',
+     'target_url': 'thunderbird/releases/16.0/win32/es-ES/Thunderbird Setup 16.0.exe'}
+]
+
+tests = firefox_tests + thunderbird_tests
+
 
 class ReleaseScraperTest(mhttpd.MozHttpdBaseTest):
     """test mozdownload scraper class"""
@@ -19,143 +127,13 @@ class ReleaseScraperTest(mhttpd.MozHttpdBaseTest):
     def test_scraper(self):
         """Testing various download scenarios for ReleaseScraper"""
 
-        ffox_release_dir = urljoin(self.wdir, 'firefox', 'releases')
-
-        firefox_tests = [
-            # -p win32 -v latest
-            {'args': {'platform': 'win32',
-                      'version': 'latest'},
-             'target': 'firefox-latest.en-US.win32.exe',
-             'target_url': urljoin(ffox_release_dir, 'latest', 'win32', 'en-US',
-                                   'Firefox Setup 23.0.1.exe')},
-            # -a firefox -p win32 -v latest
-            {'args': {'application': 'firefox',
-                      'platform': 'win32',
-                      'version': 'latest'},
-             'target': 'firefox-latest.en-US.win32.exe',
-             'target_url': urljoin(ffox_release_dir, 'latest', 'win32', 'en-US',
-                                   'Firefox Setup 23.0.1.exe')},
-            # -a firefox -p linux -v latest
-            {'args': {'application': 'firefox',
-                      'platform': 'linux',
-                      'version': 'latest'},
-             'target': 'firefox-latest.en-US.linux.tar.bz2',
-             'target_url': urljoin(ffox_release_dir, 'latest', 'linux-i686', 'en-US',
-                                   'firefox-23.en-US.linux.tar.bz2')},
-            # -a firefox -p linux64 -v latest
-            {'args': {'application': 'firefox',
-                      'platform': 'linux64',
-                      'version': 'latest'},
-             'target': 'firefox-latest.en-US.linux64.tar.bz2',
-             'target_url': urljoin(ffox_release_dir, 'latest', 'linux-x86_64', 'en-US',
-                                   'firefox-23.en-US.linux.tar.bz2')},
-            # -a firefox -p mac -v latest
-            {'args': {'application': 'firefox',
-                      'platform': 'mac',
-                      'version': 'latest'},
-             'target': 'firefox-latest.en-US.mac.dmg',
-             'target_url': urljoin(ffox_release_dir, 'latest', 'mac', 'en-US',
-                                   'firefox-23.dmg')},
-            # -a firefox -p win32 -v latest -l de
-            {'args': {'application': 'firefox',
-                      'locale': 'de',
-                      'platform': 'win32',
-                      'version': 'latest'},
-             'target': 'firefox-latest.de.win32.exe',
-             'target_url': urljoin(ffox_release_dir, 'latest', 'win32', 'de',
-                                   'Firefox Setup 23.0.1.exe')},
-            # -a firefox -p win32 -v latest --stub
-            {'args': {'application': 'firefox',
-                      'platform': 'win32',
-                      'is_stub_installer': True,
-                      'version': 'latest'},
-             'target': 'firefox-latest.en-US.win32.exe',
-             'target_url': urljoin(ffox_release_dir, 'latest', 'win32', 'en-US',
-                                   'Firefox Setup Stub 23.0.1.exe')},
-            # -a firefox -p win32 -v 21.0
-            {'args': {'application': 'firefox',
-                      'platform': 'win32',
-                      'version': '21.0'},
-             'target': 'firefox-21.0.en-US.win32.exe',
-             'target_url': urljoin(ffox_release_dir, '21.0', 'win32', 'en-US',
-                                   'Firefox Setup 21.0.exe')},
-            # -a firefox -p win32 -v 21.0 -l es-ES
-            {'args': {'application': 'firefox',
-                      'locale': 'es-ES',
-                      'platform': 'win32',
-                      'version': '21.0'},
-             'target': 'firefox-21.0.es-ES.win32.exe',
-             'target_url': urljoin(ffox_release_dir, '21.0', 'win32', 'es-ES',
-                                   'Firefox Setup 21.0.exe')}
-        ]
-
-        tbird_release_dir = urljoin(self.wdir, 'thunderbird', 'releases')
-
-        thunderbird_tests = [
-            # -a thunderbird -p win32 -v latest
-            {'args': {'application': 'thunderbird',
-                      'platform': 'win32',
-                      'version': 'latest'},
-             'target': 'thunderbird-latest.en-US.win32.exe',
-             'target_url': urljoin(tbird_release_dir, 'latest', 'win32',
-                                   'en-US', 'Thunderbird Setup 17.0.1.exe')},
-            # -a thunderbird -p linux -v latest
-            {'args': {'application': 'thunderbird',
-                      'platform': 'linux',
-                      'version': 'latest'},
-             'target': 'thunderbird-latest.en-US.linux.tar.bz2',
-             'target_url': urljoin(tbird_release_dir, 'latest', 'linux-i686',
-                                   'en-US',
-                                   'thunderbird-17.0.en-US.linux.tar.bz2')},
-            # -a thunderbird -p linux64 -v latest
-            {'args': {'application': 'thunderbird',
-                      'platform': 'linux64',
-                      'version': 'latest'},
-             'target': 'thunderbird-latest.en-US.linux64.tar.bz2',
-             'target_url': urljoin(tbird_release_dir, 'latest', 'linux-x86_64',
-                                   'en-US',
-                                   'thunderbird-17.0.en-US.linux.tar.bz2')},
-            # -a thunderbird -p mac -v latest
-            {'args': {'application': 'thunderbird',
-                      'platform': 'mac',
-                      'version': 'latest'},
-             'target': 'thunderbird-latest.en-US.mac.dmg',
-             'target_url': urljoin(tbird_release_dir, 'latest', 'mac',
-                                   'en-US', 'thunderbird-17.0.dmg')},
-            # -a thunderbird -p win32 -v latest -l de
-            {'args': {'application': 'thunderbird',
-                      'locale': 'de',
-                      'platform': 'win32',
-                      'version': 'latest'},
-             'target': 'thunderbird-latest.de.win32.exe',
-             'target_url': urljoin(tbird_release_dir, 'latest', 'win32',
-                                   'de', 'Thunderbird Setup 17.0.1.exe')},
-            # -a thunderbird -p win32 -v 16.0
-            {'args': {'application': 'thunderbird',
-                      'platform': 'win32',
-                      'version': '16.0'},
-             'target': 'thunderbird-16.0.en-US.win32.exe',
-             'target_url': urljoin(tbird_release_dir, '16.0', 'win32',
-                                   'en-US', 'Thunderbird Setup 16.0.exe')},
-            # -a thunderbird -p win32 -v 16.0 -l es-ES
-            {'args': {'application': 'thunderbird',
-                      'locale': 'es-ES',
-                      'platform': 'win32',
-                      'version': '16.0'},
-             'target': 'thunderbird-16.0.es-ES.win32.exe',
-             'target_url': urljoin(tbird_release_dir, '16.0', 'win32',
-                                   'es-ES', 'Thunderbird Setup 16.0.exe')}
-        ]
-
-        self.tests = firefox_tests + thunderbird_tests
-
-        for entry in self.tests:
+        for entry in tests:
             scraper = ReleaseScraper(directory=self.temp_dir, base_url=self.wdir,
                                      **entry['args'])
             expected_target = os.path.join(self.temp_dir, entry['target'])
             self.assertEqual(scraper.target, expected_target)
             self.assertEqual(urllib.unquote(scraper.final_url),
-                             entry['target_url'])
+                             urljoin(self.wdir, entry['target_url']))
 
 if __name__ == '__main__':
     unittest.main()
