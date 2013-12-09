f5f34e8e367c3f364977ed498432ac04f5a84cdb
Johannes Vogel <mozilla_dev@nebelhom.com>
2013-12-09 17:38:06 +0100

    Made path platform safe, changed pip import and call to subprocess.call to pip, moved 'except IOError' closer to potential source

--- run_tests.py
+++ run_tests.py
@@ -4,7 +4,6 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import os
-import pip
 from subprocess import call
 import sys
 
@@ -19,11 +18,12 @@ VERSION_VIRTUAL_ENV = '1.9.1'
 # see http://stackoverflow.com/questions/12332975/installing-python-module-within-code
 def install(package, version):
     package_arg = "%s==%s" % (package, version)
-    pip.main(['install', '--upgrade', package_arg])
+    call(['pip', 'install', '--upgrade', package_arg])
 
 
 def python(*args):
-    call(['tests/venv/bin/python'] + list(args))
+    pypath = [os.path.join(os.getcwd(), 'tests', 'venv', 'bin', 'python')]
+    call(pypath + list(args))
 
 try:
     # for more info see:
@@ -44,19 +44,20 @@ try:
     execfile(activate_this_file, dict(__file__=activate_this_file))
     print "Virtual environment activated successfully."
 
-    # Installs
-    install('ManifestDestiny', VERSION_MANIFEST_DESTINY)
-    install('mozfile', VERSION_MOZFILE)
-    install('mozhttpd', VERSION_MOZHTTPD)
-    install('mozlog', VERSION_MOZLOG)
-    install('moztest', VERSION_MOZTEST)
-
-    # Install mozdownload
-    python('setup.py', 'develop')
-    # run the tests
-    tests_path = os.path.join('tests', 'test.py')
-    python(tests_path)
-
 except IOError:
     print "Could not activate virtual environment."
     print "Exiting."
+
+# Install dependent packages
+install('ManifestDestiny', VERSION_MANIFEST_DESTINY)
+install('mozfile', VERSION_MOZFILE)
+install('mozhttpd', VERSION_MOZHTTPD)
+install('mozlog', VERSION_MOZLOG)
+install('moztest', VERSION_MOZTEST)
+
+# Install mozdownload
+python('setup.py', 'develop')
+
+# run the tests
+tests_path = os.path.join('tests', 'test.py')
+python(tests_path)
