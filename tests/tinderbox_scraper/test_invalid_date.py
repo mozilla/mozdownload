6dd9f3a1042f2a0d4e26335fc4ecec2619aeeaa9
Cosmin Malutan <cosmin.malutan@softvision.ro>
2014-01-28 14:57:25 +0200

    Check if self.timestamp in method build_filename is really of date type and not just a string (#196)

---
+++ tests/tinderbox_scraper/test_invalid_date.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+import mozlog
+import sys
+import unittest
+import urllib
+
+from mozdownload import TinderboxScraper
+from mozdownload import NotFoundError
+from mozdownload.utils import urljoin
+
+import mozhttpd_base_test as mhttpd
+
+
+# testing with an invalid date parameter should download the latest build for
+# localized builds
+tests_with_invalid_date = [
+    # -a firefox -p win32 --branch=mozilla-central --date=20140317030202
+    {'args': {'application': 'firefox',
+              'branch': 'mozilla-central',
+              'date': '20140317030202',
+              'locale': 'pt-PT',
+              'platform': 'win32'}
+    },
+    # -p win32 --branch=mozilla-central  --date=invalid
+    {'args': {'branch': 'mozilla-central',
+              'date': 'invalid',
+              'locale': 'pt-PT',
+              'platform': 'win32'}
+    },
+    # -p win64 --branch=mozilla-central --date=2013/07/02
+    {'args': {'branch': 'mozilla-central',
+              'date': '2013/07/02',
+              'platform': 'win64'},
+    },
+    # -p win32 --branch=mozilla-central --date=2013-March-15
+    {'args': {'branch': 'mozilla-central',
+              'date': '2013-March-15',
+              'platform': 'win32'},
+    }
+]
+
+tests = tests_with_invalid_date
+
+
+class TestTinderboxScraper_invalidParameters(mhttpd.MozHttpdBaseTest):
+    """test mozdownload TinderboxScraper class with invalid parameters"""
+
+    def test_scraper(self):
+        """Testing download scenarios with invalid parameters for TinderboxScraper"""
+
+        for entry in tests:
+            with self.assertRaises(ValueError):
+                TinderboxScraper(directory=self.temp_dir, version=None,
+                                 base_url=self.wdir, log_level='ERROR',
+                                 **entry['args'])
+
+
+if __name__ == '__main__':
+    unittest.main()
