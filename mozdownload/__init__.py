2793be8158cf88ced3a86a9226edc18051195f28
Timur Timirkhanov <timur.timirkhanov@gmail.com>
2015-06-10 10:12:59 +0600

    Removed wildcard import

--- mozdownload/__init__.py
+++ mozdownload/__init__.py
@@ -2,4 +2,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from scraper import * #noqa
+from scraper import PLATFORM_FRAGMENTS, NotSupportedError, NotFoundError, \
+    NotImplementedError, TimeoutError, Scraper, DailyScraper, \
+    DirectScraper, ReleaseScraper, ReleaseCandidateScraper, \
+    TinderboxScraper, TryScraper
+from parser import DirectoryParser
+from timezones import PacificTimezone
+
+__all__ = [PLATFORM_FRAGMENTS, NotSupportedError, NotFoundError,
+           NotImplementedError, TimeoutError, Scraper,
+           DailyScraper, DirectScraper, ReleaseScraper,
+           ReleaseCandidateScraper, TinderboxScraper, TryScraper,
+           DirectoryParser, PacificTimezone]
