1057ab1c7abc133f2217627cd3f02513573346f4
Adam Schmalhofer <Adam.Schmalhofer@gmx.de>
2018-10-17 21:57:28 +0200

    Fix code comment

--- tests/remote/test_devedition.py
+++ tests/remote/test_devedition.py
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-"""Test all scraper classes for Firefox against the remote server"""
+"""Test all scraper classes for Firefox Developer Edition against the remote server"""
 
 import urllib
 
