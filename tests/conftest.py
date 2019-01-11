da70763d8cd669f2e93aa40590909f02c64efcd5
Utkarsh Sharma <f20160600@Goa.bits-pilani.ac.in>
2019-01-11 00:14:50 -0800

    Change test_release_candidate_scraper_indices.py to pytest #468
    Removed custom wdir variable and added get_url method inside the tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
+import os
 import pytest
 
-import os
 from wptserve import server
 
 
@@ -10,8 +10,4 @@ def httpd():
     httpd = server.WebTestHttpd(port=8080,
                                         doc_root=os.path.join(HERE, 'data'),
                                         host='127.0.0.1')
-    global wdir
-    wdir = 'http://{}:{}'.format(httpd.host, httpd.port)
-    sv = httpd.start(block=False)
-    yield sv
-    httpd.stop()
+    return httpd
--- tests/release_candidate_scraper/test_release_candidate_scraper_indices.py
+++ tests/release_candidate_scraper/test_release_candidate_scraper_indices.py
@@ -3,11 +3,13 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
+import pytest
+
+from wptserve import server
 
 from mozdownload import ReleaseCandidateScraper
 import conftest
 
-import pytest
 
 
 @pytest.mark.parametrize("args,build_index,build_number,builds", [
@@ -22,7 +24,11 @@ import pytest
 def test_build_indices(httpd, tmpdir, args, build_index, build_number, builds):
     """Testing indices in choosing builds for ReleaseCandidateScraper"""
 
-    scraper = ReleaseCandidateScraper(destination=tmpdir, base_url=conftest.wdir, **args)
+    return httpd.start(block=False)
+
+    scraper = ReleaseCandidateScraper(destination=tmpdir, base_url=httpd.get_url(), **args)
     assert scraper.build_index == int(build_index)
     assert scraper.build_number == build_number
     assert scraper.builds == builds
+
+    httpd.stop()
