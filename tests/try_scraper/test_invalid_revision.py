d609044202f19cfbd700e77c3d92c2e8fb9c00ee
Rishi <rishigpt2009@gmail.com>
2019-01-23 04:44:42 +0530

    TestScraper: port unittest to pytest #468

--- tests/try_scraper/test_invalid_revision.py
+++ tests/try_scraper/test_invalid_revision.py
@@ -5,24 +5,18 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from mock import patch
-
+import pytest
 from mozdownload import TryScraper
 import mozdownload.errors as errors
-import mozhttpd_base_test as mhttpd
 
 
-class TestTryScraperInvalidParameters(mhttpd.MozHttpdBaseTest):
-    """test mozdownload TryScraper class with invalid parameters"""
+@patch('mozdownload.treeherder.Treeherder.query_builds_by_revision')
+def test_scraper(query_builds_by_revision, httpd, tmpdir):
+    """Testing download scenarios with invalid parameters for TryScraper"""
+    query_builds_by_revision.return_value = []
 
-    @patch('mozdownload.treeherder.Treeherder.query_builds_by_revision')
-    def test_scraper(self, query_builds_by_revision):
-        """Testing download scenarios with invalid parameters for TryScraper"""
-        query_builds_by_revision.return_value = []
-
-        with self.assertRaises(errors.NotFoundError):
-            TryScraper(destination=self.temp_dir,
-                       base_url=self.wdir,
-                       logger=self.logger,
-                       platform='win32',
-                       revision='abc'
-                       )
+    with pytest.raises(errors.NotFoundError):
+        assert TryScraper(destination=tmpdir,
+                          base_url=httpd.get_url(),
+                          platform='win32',
+                          revision='abc')
