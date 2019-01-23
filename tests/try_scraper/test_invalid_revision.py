b9803f43fb4b67fccf70872757f55863fc78c22d
Rishi <rishigpt2009@gmail.com>
2019-01-23 23:48:41 +0530

    removing unnecessary blacnk lines and comments, used pytest-mock inplace of mock, renaming of function #468

--- tests/try_scraper/test_invalid_revision.py
+++ tests/try_scraper/test_invalid_revision.py
@@ -4,17 +4,14 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from mock import patch
 import pytest
 from mozdownload import TryScraper
 import mozdownload.errors as errors
 
-
-@patch('mozdownload.treeherder.Treeherder.query_builds_by_revision')
-def test_scraper(query_builds_by_revision, httpd, tmpdir):
+def test_invalid_parameters(mocker, httpd, tmpdir):
     """Testing download scenarios with invalid parameters for TryScraper"""
+    query_builds_by_revision = mocker.patch('mozdownload.treeherder.Treeherder.query_builds_by_revision')
     query_builds_by_revision.return_value = []
-
     with pytest.raises(errors.NotFoundError):
         assert TryScraper(destination=tmpdir,
                           base_url=httpd.get_url(),
--- tests/try_scraper/test_try_scraper.py
+++ tests/try_scraper/test_try_scraper.py
@@ -7,54 +7,38 @@
 import os
 import urllib
 import pytest
-from mock import patch
 from mozdownload import TryScraper
 from mozdownload.utils import urljoin
 
 @pytest.mark.parametrize("args,filename,url", [
-    # -p mac64 --revision=8fcac92cfcad
     ({'platform': 'mac64', 'revision': '8fcac92cfcad'},
      '8fcac92cfcad-firefox-38.0a1.en-US.mac.dmg',
-     'firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/'
-     'try-macosx64/firefox-38.0a1.en-US.mac.dmg'),
-    # -p mac --revision=8fcac92cfcad
+     'firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/try-macosx64/firefox-38.0a1.en-US.mac.dmg'),
     ({'platform': 'mac', 'revision': '8fcac92cfcad'},
      '8fcac92cfcad-firefox-38.0a1.en-US.mac.dmg',
-     'firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/'
-     'try-macosx64/firefox-38.0a1.en-US.mac.dmg'),
-    # -a firefox -p linux64 --revision=8fcac92cfcad
+     'firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/try-macosx64/firefox-38.0a1.en-US.mac.dmg'),
     ({'platform': 'linux64', 'revision': '8fcac92cfcad'},
      '8fcac92cfcad-firefox-38.0a1.en-US.linux-x86_64.tar.bz2',
-     'firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/'
-     'try-linux64/firefox-38.0a1.en-US.linux-x86_64.tar.bz2'),
-    # -a firefox -p linux --revision=8fcac92cfcad --debug-build
+     'firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/try-linux64/firefox-38.0a1.en-US.linux-x86_64.tar.bz2'),
     ({'platform': 'linux', 'revision': '8fcac92cfcad', 'debug_build': True},
      '8fcac92cfcad-debug-firefox-38.0a1.en-US.linux-i686.tar.bz2',
-     'firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/'
-     'try-linux-debug/firefox-38.0a1.en-US.linux-i686.tar.bz2'),
-    # -a firefox -p win32 --revision=8fcac92cfcad
+     'firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/try-linux-debug/firefox-38.0a1.en-US.linux-i686.tar.bz2'),
     ({'platform': 'win32', 'revision': '8fcac92cfcad'},
      '8fcac92cfcad-firefox-38.0a1.en-US.win32.installer.exe',
-     'firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/'
-     'try-win32/firefox-38.0a1.en-US.win32.installer.exe'),
-    # -a firefox -p win64 --revision=8fcac92cfcad
+     'firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/try-win32/firefox-38.0a1.en-US.win32.installer.exe'),
     ({'platform': 'win64', 'revision': '8fcac92cfcad'},
      '8fcac92cfcad-firefox-38.0a1.en-US.win64.installer.exe',
-     'firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/'
-     'try-win64/firefox-38.0a1.en-US.win64.installer.exe')
+     'firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/try-win64/firefox-38.0a1.en-US.win64.installer.exe')
 ])
-
-
-@patch('mozdownload.treeherder.Treeherder.query_builds_by_revision')
-def test_scraper(mock_query_builds_by_revision, httpd, tmpdir, args, filename, url):
+def test_scraper(mocker, httpd, tmpdir, args, filename, url):
     """Testing various download scenarios for TryScraper"""
-    mock_query_builds_by_revision.return_value = [
+    query_builds_by_revision = mocker.patch('mozdownload.treeherder.Treeherder.query_builds_by_revision')
+    query_builds_by_revision.return_value = [
         '/firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/try-foobar/'
     ]
-    baseurl = httpd.get_url()
     scraper = TryScraper(destination=str(tmpdir),
-                         base_url=baseurl,
+                         base_url=httpd.get_url(),
                          **args)
     expected_filename = os.path.join(str(tmpdir), filename)
     assert scraper.filename == expected_filename
-    assert urllib.unquote(scraper.url) == urljoin(baseurl, url)
+    assert urllib.unquote(scraper.url) == urljoin(httpd.get_url(), url)
