1855d0808569c24e5932703926bb9f10e1c19b4d
Rishi <rishigpt2009@gmail.com>
2019-01-24 23:09:44 +0530

    strict version of pytest-mock lib, removing assert #468

--- tests/try_scraper/test_invalid_revision.py
+++ tests/try_scraper/test_invalid_revision.py
@@ -13,7 +13,7 @@ def test_invalid_parameters(httpd, tmpdir, mocker):
     query_builds_by_revision = mocker.patch('mozdownload.treeherder.Treeherder.query_builds_by_revision')
     query_builds_by_revision.return_value = []
     with pytest.raises(errors.NotFoundError):
-        assert TryScraper(destination=tmpdir,
-                          base_url=httpd.get_url(),
-                          platform='win32',
-                          revision='abc')
+        TryScraper(destination=tmpdir,
+                   base_url=httpd.get_url(),
+                   platform='win32',
+                   revision='abc')
