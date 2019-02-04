63d45227fc92e3dc239c63500adb175c91400013
Utkarsh Sharma <f20160600@Goa.bits-pilani.ac.in>
2019-02-04 10:07:19 -0800

    Convert tinderbox_scraper tests to pytest#468

--- tests/tinderbox_scraper/test_invalid_date_tinderbox.py
+++ tests/tinderbox_scraper/test_invalid_date_tinderbox.py
@@ -6,45 +6,19 @@
 
 from mozdownload import TinderboxScraper
 
-import mozhttpd_base_test as mhttpd
+import pytest
 
 
-# testing with an invalid date parameter should download the latest build for
-# localized builds
-tests_with_invalid_date = [
-    # -a firefox -p win32 --branch=mozilla-central --date=20140317030202
-    {'args': {'application': 'firefox',
-              'branch': 'mozilla-central',
-              'date': '20140317030202',
-              'locale': 'pt-PT',
-              'platform': 'win32'}},
-    # -p win32 --branch=mozilla-central  --date=invalid
-    {'args': {'branch': 'mozilla-central',
-              'date': 'invalid',
-              'locale': 'pt-PT',
-              'platform': 'win32'}},
-    # -p win64 --branch=mozilla-central --date=2013/07/02
-    {'args': {'branch': 'mozilla-central',
-              'date': '2013/07/02',
-              'platform': 'win64'}},
-    # -p win32 --branch=mozilla-central --date=2013-March-15
-    {'args': {'branch': 'mozilla-central',
-              'date': '2013-March-15',
-              'platform': 'win32'}}
-]
+@pytest.mark.parametrize("args", [
+    ({'application': 'firefox', 'branch': 'mozilla-central', 'date': '20140317030202',
+    'locale': 'pt-PT', 'platform': 'win32'}),
+    ({'branch': 'mozilla-central', 'date': 'invalid', 'locale': 'pt-PT', 'platform': 'win32'}),
+    ({'branch': 'mozilla-central', 'date': '2013/07/02', 'platform': 'win64'}),
+    ({'branch': 'mozilla-central', 'date': '2013-March-15', 'platform': 'win32'}),
+])
 
-tests = tests_with_invalid_date
+def test_scraper(httpd, tmpdir, args):
+    """Testing download scenarios with invalid parameters for TinderboxScraper"""
 
-
-class TestTinderboxScraperInvalidParameters(mhttpd.MozHttpdBaseTest):
-    """test mozdownload TinderboxScraper class with invalid parameters"""
-
-    def test_scraper(self):
-        """Testing download scenarios with invalid parameters for TinderboxScraper"""
-
-        for entry in tests:
-            self.assertRaises(ValueError, TinderboxScraper,
-                              destination=self.temp_dir,
-                              base_url=self.wdir,
-                              logger=self.logger,
-                              **entry['args'])
+    with pytest.raises(ValueError):
+        TinderboxScraper(destination=str(tmpdir), base_url=httpd.get_url(), **args)
