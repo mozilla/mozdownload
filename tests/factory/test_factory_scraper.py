849b6a624e10e4702fc0f225bc06c782296e9db3
Rishi <rishigpt2009@gmail.com>
2019-03-08 07:49:39 +0530

    reincorporating the old paramterization strucuture - #468

--- tests/factory/test_factory_scraper.py
+++ tests/factory/test_factory_scraper.py
@@ -6,57 +6,38 @@ import pytest
 from mozdownload import FactoryScraper
 
 
-@pytest.mark.parametrize('filename,args', [
+@pytest.mark.parametrize('scraper_type,builds,filename,args', [
     # ReleaseScraper
-    ('release',
-     {
-         'fname': 'firefox-23.0.1.en-US.win32.exe',
-         'kwargs': {
-             'platform': 'win32',
-             'version': '23.0.1',
-         }
-     }),
+    ('release', None, 'firefox-23.0.1.en-US.win32.exe', {
+        'platform': 'win32',
+        'version': '23.0.1',
+    }),
 
     # ReleaseCandidateScraper
-    ('candidate',
-     {
-         'fname': 'firefox-23.0.1-build3.en-US.win32.exe',
-         'kwargs': {
-             'platform': 'win32',
-             'version': '23.0.1',
-         }
-     }),
+    ('candidate', None, 'firefox-23.0.1-build3.en-US.win32.exe', {
+        'platform': 'win32',
+        'version': '23.0.1',
+    }),
 
     # DailyScraper
-    ('daily',
-     {
-         'fname': '2013-10-01-03-02-04-mozilla-central-firefox-27.0a1.en-US.win32.installer.exe',
-         'kwargs': {
-             'platform': 'win32',
-         }
+    ('daily', None,
+     '2013-10-01-03-02-04-mozilla-central-firefox-27.0a1.en-US.win32.installer.exe', {
+         'platform': 'win32',
      }),
 
     # TinderboxScraper
-    ('tinderbox',
-     {
-         'fname': '1374583608-mozilla-central-firefox-25.0a1.en-US.win32.installer.exe',
-         'kwargs': {
-             'platform': 'win32',
-         }
-     }),
+    ('tinderbox', None, '1374583608-mozilla-central-firefox-25.0a1.en-US.win32.installer.exe', {
+        'platform': 'win32',
+    }),
 
     # TryScraper
-    ('try',
-     {
-         'builds': ['/firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/try-foobar/'],
-         'fname': '8fcac92cfcad-firefox-38.0a1.en-US.mac.dmg',
-         'kwargs': {
-             'revision': '8fcac92cfcad',
-             'platform': 'mac64',
-         }
+    ('try', ['/firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/try-foobar/'],
+     '8fcac92cfcad-firefox-38.0a1.en-US.mac.dmg', {
+         'revision': '8fcac92cfcad',
+         'platform': 'mac64',
      }),
 ])
-def test_factory(httpd, tmpdir, filename, args, mocker):
+def test_factory(httpd, tmpdir, scraper_type, builds, filename, args, mocker):
     """Testing various download scenarios for the factory."""
     query_builds_by_revision \
         = mocker.patch('mozdownload.treeherder.Treeherder.query_builds_by_revision')
@@ -66,14 +47,14 @@ def test_factory(httpd, tmpdir, filename, args, mocker):
         'log_level': 'ERROR',
     }
 
-    if args.get('builds'):
-        query_builds_by_revision.return_value = args.get('builds')
+    if builds:
+        query_builds_by_revision.return_value = builds
 
     kwargs = copy.deepcopy(base_kwargs)
-    kwargs.update(args.get('kwargs'))
+    kwargs.update(args)
 
-    build = FactoryScraper(filename, **kwargs)
+    build = FactoryScraper(scraper_type, **kwargs)
     build.download()
 
     dir_content = os.listdir(str(tmpdir))
-    assert args['fname'] in dir_content
+    assert filename in dir_content
--- tests/factory/test_factory_unused_options.py
+++ tests/factory/test_factory_unused_options.py
@@ -7,84 +7,65 @@ from mozdownload.utils import urljoin
 from mozdownload import FactoryScraper
 
 
-@pytest.mark.parametrize('filename,args', [
+@pytest.mark.parametrize('scraper_type,builds,filename,args', [
     # ReleaseScraper
-    ('release',
-     {
-         'fname': 'firefox-23.0.1.en-US.win32.exe',
-         'kwargs': {
-             'version': '23.0.1',
-             'platform': 'win32',
-             # unused options
-             'branch': 'mozilla-central',
-             'build_id': '20131001030204',
-             'build_number': '1',
-             'revision': '8fcac92cfcad',
-             'date': '2013-10-01',
-             'debug_build': True,
-             'url': 'http://localhost',
-         }
-     }),
+    ('release', None, 'firefox-23.0.1.en-US.win32.exe', {
+        'version': '23.0.1',
+        'platform': 'win32',
+        # unused options
+        'branch': 'mozilla-central',
+        'build_id': '20131001030204',
+        'build_number': '1',
+        'revision': '8fcac92cfcad',
+        'date': '2013-10-01',
+        'debug_build': True,
+        'url': 'http://localhost',
+    }),
 
     # ReleaseCandidateScraper
-    ('candidate',
-     {
-         'fname': 'firefox-23.0.1-build3.en-US.win32.exe',
-         'kwargs': {
-             'platform': 'win32',
-             'version': '23.0.1',
-             # unused options
-             'branch': 'mozilla-central',
-             'build_id': '20131001030204',
-             'revision': '8fcac92cfcad',
-             'date': '2013-10-01',
-             'debug_build': True,
-             'url': 'http://localhost',
-         }
-     }),
+    ('candidate', None, 'firefox-23.0.1-build3.en-US.win32.exe', {
+        'platform': 'win32',
+        'version': '23.0.1',
+        # unused options
+        'branch': 'mozilla-central',
+        'build_id': '20131001030204',
+        'revision': '8fcac92cfcad',
+        'date': '2013-10-01',
+        'debug_build': True,
+        'url': 'http://localhost',
+    }),
 
     # DailyScraper
-    ('daily',
-     {
-         'fname': '2013-10-01-03-02-04-mozilla-central-firefox-27.0a1.en-US.win32.installer.exe',
-         'kwargs': {
-             'platform': 'win32',
-             # unused options
-             'debug_build': True,
-             'url': 'http://localhost',
-             'version': '23.0.1',
-         }
+    ('daily', None,
+     '2013-10-01-03-02-04-mozilla-central-firefox-27.0a1.en-US.win32.installer.exe', {
+         'platform': 'win32',
+         # unused options
+         'debug_build': True,
+         'url': 'http://localhost',
+         'version': '23.0.1',
      }),
 
     # TinderboxScraper
-    ('tinderbox',
-     {
-         'fname': '1374583608-mozilla-central-firefox-25.0a1.en-US.win32.installer.exe',
-         'kwargs': {
-             'platform': 'win32',
-             # unused options
-             'url': 'http://localhost',
-             'version': '23.0.1',
-         }
-     }),
+    ('tinderbox', None, '1374583608-mozilla-central-firefox-25.0a1.en-US.win32.installer.exe', {
+        'platform': 'win32',
+        # unused options
+        'url': 'http://localhost',
+        'version': '23.0.1',
+    }),
 
     # TryScraper
-    ('try',
-     {
-         'builds': ['/firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/try-foobar/'],
-         'fname': '8fcac92cfcad-firefox-38.0a1.en-US.mac.dmg',
-         'kwargs': {
-             'revision': '8fcac92cfcad',
-             'platform': 'mac64',
-             # unused options
-             'build_id': '20131001030204',
-             'build_number': '1',
-             'url': 'http://localhost',
-             'version': '23.0.1',
-         }
+    ('try', ['/firefox/try-builds/test-user@mozilla.com-8fcac92cfcad/try-foobar/'],
+     '8fcac92cfcad-firefox-38.0a1.en-US.mac.dmg', {
+         'revision': '8fcac92cfcad',
+         'platform': 'mac64',
+         # unused options
+         'build_id': '20131001030204',
+         'build_number': '1',
+         'url': 'http://localhost',
+         'version': '23.0.1',
      })
 ])
-def test_unused_options(httpd, tmpdir, filename, args, mocker):
+def test_unused_options(httpd, tmpdir, scraper_type, builds, filename, args, mocker):
     """Test that unknown optional options do not break the given scraper."""
     base_kwargs = {
         'base_url': httpd.get_url(),
@@ -94,17 +75,17 @@ def test_unused_options(httpd, tmpdir, filename, args, mocker):
     query_builds_by_revision = \
         mocker.patch('mozdownload.treeherder.Treeherder.query_builds_by_revision')
 
-    if args.get('builds'):
-        query_builds_by_revision.return_value = args.get('builds')
+    if builds:
+        query_builds_by_revision.return_value = builds
 
     kwargs = copy.deepcopy(base_kwargs)
-    kwargs.update(args.get('kwargs'))
+    kwargs.update(args)
 
-    build = FactoryScraper(filename, **kwargs)
+    build = FactoryScraper(scraper_type, **kwargs)
     build.download()
 
     dir_content = os.listdir(str(tmpdir))
-    assert args['fname'] in dir_content
+    assert filename in dir_content
 
 
 def test_unused_options_direct_scraper(httpd, tmpdir):
