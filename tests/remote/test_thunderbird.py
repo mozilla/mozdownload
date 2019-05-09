0a157718a664a0bc9b8d24e0ea2c0a80ce1b843a
André Heringer <andregomesh@dcc.ufmg.br>
2019-05-09 03:30:31 -0300

    Skip remote tests for Thunderbird tinderbox builds (#554)

--- tests/remote/test_thunderbird.py
+++ tests/remote/test_thunderbird.py
@@ -85,9 +85,11 @@ def test_daily_scraper(tmpdir, args):
     {'application': 'thunderbird', 'branch': 'comm-central', 'platform': 'mac'},
     # Currently no debug builds are shipped for comm-central
     # {'application': 'thunderbird', 'branch': 'comm-central', 'platform': 'win32', 'debug_build': True},
-    {'application': 'thunderbird', 'branch': 'comm-central', 'platform': 'win32', 'locale': 'de'},
+    # Test inappropriately causes an xpass even when it should fail
+    # {'application': 'thunderbird', 'branch': 'comm-central', 'platform': 'win32', 'locale': 'de'},
     {'application': 'thunderbird', 'branch': 'comm-central', 'platform': 'win32', 'extension': 'txt'},
 ])
+@pytest.mark.xfail(strict=True, reason="tinderbox builds not available in the archive")
 def test_tinderbox_scraper(tmpdir, args):
     """Test tinderbox scraper against the remote server."""
     mozdownload.TinderboxScraper(destination=tmpdir, **args)
