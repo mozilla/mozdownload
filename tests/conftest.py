66d439b2113a4c028284278a5f89ea287225bb2c
Kaushik S Kalmady <19279785+kaushiksk@users.noreply.github.com>
2020-04-01 02:14:07 +0530

    Enable authentication tests in test_base_scraper.py (#597)
    - Adds a custom auth handler and register a route
    - Overrides the `decode_basic` method of request.Authentication
      -  uses the right base64 decode function and fixes the bytes/str error
    - enables the auth tests
    
    * Bump up wptserve to version 3.0

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,51 @@
+import base64
 import os
 
 import pytest
+from six.moves.urllib.parse import parse_qs, urlparse
 
-from wptserve import server
+from wptserve import (
+    handlers,
+    request,
+    routes as default_routes,
+    server,
+)
+
+
+@handlers.handler
+def basic_auth_handler(req, response):
+    # Allow the test to specify the username and password
+
+    username = b"mozilla"
+    password = b"mozilla"
+
+    auth = request.Authentication(req.headers)
+    content = """<!doctype html>
+        <title>HTTP Authentication</title>
+        <p id="status">{}</p>"""
+
+    if auth.username == username and auth.password == password:
+        response.status = 200
+        response.content = content.format("success")
+
+    else:
+        response.status = 401
+        response.headers.set("WWW-Authenticate", "Basic realm=\"secret\"")
+        response.content = content.format("restricted")
 
 
 @pytest.fixture
 def httpd():
     HERE = os.path.dirname(os.path.abspath(__file__))
+    routes = [
+        ("GET", "/basic_auth", basic_auth_handler),
+    ]
+    routes.extend(default_routes.routes)
     httpd = server.WebTestHttpd(
         host="127.0.0.1",
         port=8080,
         doc_root=os.path.join(HERE, 'data'),
+        routes=routes,
         )
     httpd.start(block=False)
     yield httpd
