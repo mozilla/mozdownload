2c5c1d6865c851b7fb81831ead7e7caa232e975f
Piriyakorn P <piriyakorn.piriyatamwong@inf.ethz.ch>
2024-05-03 19:05:21 +0200

    Add Thunderbird (and Firefox) daily default branch handling (#247)

--- mozdownload/cli.py
+++ mozdownload/cli.py
@@ -128,7 +128,6 @@ def parse_arguments(argv):
     group = parser.add_argument_group('Daily builds', 'Extra options for daily builds.')
     group.add_argument('--branch',
                        dest='branch',
-                       default='mozilla-central',
                        metavar='BRANCH',
                        help='Name of the branch, default: "%(default)s"')
     group.add_argument('--build-id',
